600748626a5478bf7d1b9e7a6fea944674acf93f
Andrew Jones <andrew.jones@puppet.com>
2023-06-29 19:01:03 +0100

    Update site.pp
    Quiet!

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,9 +26,6 @@ File { backup => false }
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
   # We're not using this, use site-modules/profile/manifests/master/nodegroups.pp instead.
-  notify { 'A notification':
-    message => $migration_server,
-  }
 }
 
 
