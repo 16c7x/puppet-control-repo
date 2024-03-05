30d5b798c396dde8929aef3b7f107659fe7fde6a
Andrew Jones <andrew.jones@puppet.com>
2024-03-05 15:43:12 +0000

    messing about with plans

--- site-modules/miscellanea/tasks/root_space.sh
+++ site-modules/miscellanea/tasks/root_space.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 rootspace=`df $PWD | awk '/[0-9]%/{print $(NF-2)}'`
 
-if [ $rootspace -gt 1000000 ]; then
+if [ $rootspace -gt 5000000000 ]; then
   result=0
 else
   result=1
