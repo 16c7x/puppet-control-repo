515b90097c350ff2425c6e76c1bd7433a30c2df9
Andrew Jones <andrew.jones@puppet.com>
2024-03-05 16:26:19 +0000

    messing about with plans

--- site-modules/miscellanea/plans/upgrade.pp
+++ site-modules/miscellanea/plans/upgrade.pp
@@ -8,7 +8,7 @@ plan miscellanea::upgrade(
     $stack_name = $item.status
   }
 
-  return($rootstatus)
+  return($rootstatus[0])
 
   if $stack_status == 'fail' {
     fail_plan('Root partition is full')
