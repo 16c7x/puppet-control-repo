896b0480f8f8dc022ee3b743beba1fb8b1daa8ad
Andrew Jones <andrew.jones@puppet.com>
2024-03-06 13:08:49 +0000

    messing about with plans

--- site-modules/miscellanea/plans/upgrade.pp
+++ site-modules/miscellanea/plans/upgrade.pp
@@ -3,25 +3,12 @@ plan miscellanea::upgrade(
 ) {
   $results = run_task('miscellanea::root_space', $targets)
 
-  #$subset = $results.map |$result| {
-  #  $result.value
-  #}
-
   $pass_output = $results.filter |$item| { $item['_output'] =~ /pass/ }
 
-  $pass_value = $pass_output.map |$item| { $item.target }
+  $pass_list = $pass_output.map |$item| { $item.target }
 
-  #$answered_true = $results.filter |$result| { $result[] == true }
+  #return($pass_list)
 
-  return($pass_value)
+  $patchstatus = run_task('miscellanea::yum_update', $pass_list)
 
-  if $stack_status == 'fail' {
-    fail_plan('Root partition is full')
-  }
-
-  $patchstatus = run_task('miscellanea::yum_update', $targets)
-
-  if $patchstatus!= 0 {
-    fail_plan('patching failed')
-  }
 }
