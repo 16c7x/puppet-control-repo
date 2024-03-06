7ddf3268a2d600be261d9c8782c642a5d1dc264e
Andrew Jones <andrew.jones@puppet.com>
2024-03-06 13:26:08 +0000

    messing about with plans

--- site-modules/miscellanea/plans/upgrade.pp
+++ site-modules/miscellanea/plans/upgrade.pp
@@ -1,14 +1,16 @@
 plan miscellanea::upgrade(
   TargetSpec $targets,
 ) {
+  # Run a test on each machine in the list to see if it has enough disk space
   $results = run_task('miscellanea::root_space', $targets)
 
+  # Filter out the result sets of the nodes that passed the test
   $pass_output = $results.filter |$item| { $item['_output'] =~ /pass/ }
 
+  # Extract the hostnames from the result set of nodes that passed
   $pass_list = $pass_output.map |$item| { $item.target }
 
-  #return($pass_list)
-
+  # Run the patching task on the list of nodes that passed the test
   $patchstatus = run_task('miscellanea::yum_update', $pass_list)
 
 }
