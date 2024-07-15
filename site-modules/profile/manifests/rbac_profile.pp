4b6dd3330dca76f4cb5ca54af42cba9966fbe332
Andrew Jones <andrew.jones@puppet.com>
2024-07-15 15:37:42 +0100

    Create rbac_profile.pp

---
+++ site-modules/profile/manifests/rbac_profile.pp
@@ -0,0 +1,32 @@
+#
+class profile::rbac_profile {
+  rbac_user { 'testing account':
+    ensure       => present,
+    name         => 'testing',
+    display_name => 'Just a testing account',
+    email        => 'testing@puppetlabs.com',
+    password     => 'puppetlabs',
+    roles        => [ 1,2 ],
+  }
+
+  rbac_role { 'Viewers':
+    ensure      => 'present',
+    description => 'Viewers',
+    permissions => [
+    {
+      'object_type' => 'nodes',
+      'action' => 'view_data',
+      'instance' => '*'
+    },
+    {
+      'object_type' => 'console_page',
+      'action' => 'view',
+      'instance' => '*'
+    }],
+  }
+
+  rbac_group { 'admins':
+    ensure => 'present',
+    roles  => ['Administrators'],
+  }
+}
