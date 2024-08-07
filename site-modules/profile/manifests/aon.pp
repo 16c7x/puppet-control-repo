5c594dc7578fb071cd170d8b499abb34702a2a62
Andrew Jones <andrew.jones@puppet.com>
2024-08-07 18:12:28 +0100

    testing hiera merge

--- site-modules/profile/manifests/aon.pp
+++ site-modules/profile/manifests/aon.pp
@@ -1,15 +1,15 @@
 # 
 class profile::aon {
   $required_packages = lookup('required_packages', { 'merge' => 'unique' })
-  $excluded_packages = lookup('excluded_packages', { 'merge' => 'unique', 'default_value' => undef })
+  #$excluded_packages = lookup('excluded_packages', { 'merge' => 'unique', 'default_value' => undef })
 
   package { $required_packages:
     ensure => 'installed',
   }
 
-  unless $excluded_packages == undef {
-    package { $excluded_packages:
-      ensure => 'absent',
-    }
-  }
+  #unless $excluded_packages == undef {
+  #  package { $excluded_packages:
+  #    ensure => 'absent',
+  #  }
+  #}
 }
