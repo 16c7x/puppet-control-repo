4557b8e5814d7e05aaf6e17947c6b14690a5e5be
Andrew Jones <andrew.jones@puppet.com>
2025-01-21 13:31:14 +0000

    Update dbprofile.pp

--- site-modules/profile/manifests/dbprofile.pp
+++ site-modules/profile/manifests/dbprofile.pp
@@ -2,7 +2,7 @@
 class profile::dbprofile {
   if ::db_env == 'prod' {
     notify { 'A':}
-  } elsif Facts['db_env'] == 'non-prod' {
+  } elsif ::db_env == 'non-prod' {
     notify { 'B':}
   } else {
     fail('Unknown datacentre')
