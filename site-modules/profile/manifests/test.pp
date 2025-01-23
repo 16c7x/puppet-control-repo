da66c20aa04df6adc66f342af19da5775ae61923
Andrew Jones <andrew.jones@puppet.com>
2025-01-23 16:37:34 +0000

    figuring stuff out

--- site-modules/profile/manifests/test.pp
+++ site-modules/profile/manifests/test.pp
@@ -1,41 +1,4 @@
 #
-class profile::test {
-  if $facts['datacentre'] == 'A' {
-    $source = ['216.239.35.0']
-  } else {
-    $source = ['216.239.35.1']
-  }
-
-  class { 'ntp':
-    servers => $source,
-  }
-}
-
-
-
-
-#
-class profile::test {
-  if $facts['datacentre'] == 'A' {
-    $source = ['216.239.35.0']
-  } elseif $facts['datacentre'] == 'B' {
-    $source = ['216.239.35.1']
-  } elseif $facts['datacentre'] == 'C' {
-    $source = ['216.239.35.3']
-  } elseif $facts['datacentre'] == 'D' {
-    $source = ['216.239.35.4']
-  } elseif $facts['datacentre'] == 'E' {
-    $source = ['216.239.35.5']
-  } elseif $facts['datacentre'] == 'F' {
-    $source = ['216.239.35.6']
-  }
-
-  class { 'ntp':
-    servers => $source,
-  }
-}
-
-#
 class profile::test (
   $source
 ) {
