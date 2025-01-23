7b0a7069ede27f3bf9a8cdb49b26eb78f384f953
Andrew Jones <andrew.jones@puppet.com>
2025-01-23 16:33:39 +0000

    figuring stuff out

--- site-modules/profile/manifests/test.pp
+++ site-modules/profile/manifests/test.pp
@@ -1,21 +1,45 @@
 #
 class profile::test {
-  package { 'ntp':
-    ensure => installed,
-  }
-
   if $facts['datacentre'] == 'A' {
-    $source = '216.239.35.0'
+    $source = ['216.239.35.0']
   } else {
-    $source = '216.239.35.1'
+    $source = ['216.239.35.1']
   }
 
-  file { '/etc/ntp.conf':
-    content => "server ${source} iburst",
+  class { 'ntp':
+    servers => $source,
+  }
+}
+
+
+
+
+#
+class profile::test {
+  if $facts['datacentre'] == 'A' {
+    $source = ['216.239.35.0']
+  } elseif $facts['datacentre'] == 'B' {
+    $source = ['216.239.35.1']
+  } elseif $facts['datacentre'] == 'C' {
+    $source = ['216.239.35.3']
+  } elseif $facts['datacentre'] == 'D' {
+    $source = ['216.239.35.4']
+  } elseif $facts['datacentre'] == 'E' {
+    $source = ['216.239.35.5']
+  } elseif $facts['datacentre'] == 'F' {
+    $source = ['216.239.35.6']
   }
 
-  service { 'ntp':
-    ensure => running,
-    enable => true,
+  class { 'ntp':
+    servers => $source,
+  }
+}
+
+#
+class profile::test (
+  $source
+) {
+  class { 'ntp':
+    servers => $source,
   }
 }
