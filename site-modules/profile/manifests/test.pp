d02fa65f9615f54436c1971879d8d5e3df900ce3
Andrew Jones <andrew.jones@puppet.com>
2025-01-23 15:02:21 +0000

    figuring stuff out

--- site-modules/profile/manifests/test.pp
+++ site-modules/profile/manifests/test.pp
@@ -1,9 +1,9 @@
 #
 class profile::test {
   if $facts['datacentre'] == 'A' {
-    $source = '10.1.1.0'
+    $source = ['10.1.1.0']
   } else {
-    $source = '10.2.1.0'
+    $source = ['10.2.1.0']
   }
 
   class { 'ntp':
