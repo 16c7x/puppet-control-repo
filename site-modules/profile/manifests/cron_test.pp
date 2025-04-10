d565b2b99a33b61f6bb9f08519fb11ac2af6098b
Andrew Jones <andrew.jones@puppet.com>
2025-04-10 10:20:27 +0100

    Update cron_test.pp

--- site-modules/profile/manifests/cron_test.pp
+++ site-modules/profile/manifests/cron_test.pp
@@ -1,8 +1,16 @@
 # 
-class profile::cron_test {
+class profile::cron_test (
+  $minute = 20,
+){
   cron { 'test file 1':
     command => '/usr/bin/touch /tmp/test1',
     user    => 'root',
     minute  => 15,
+  }
+
+  cron { 'test file 2':
+    command => '/usr/bin/touch /tmp/test2',
+    user    => 'root',
+    minute  => $minute,
   }
 }
