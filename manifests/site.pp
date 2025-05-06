21f830605843f2304d8aefe4f27779384a953eb4
Andrew Jones <andrew.jones@puppet.com>
2025-05-06 12:59:06 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,7 +20,7 @@ File { backup => false }
 
 ## Node Definitions ##
 
-$participant = 'acme_corp'
+#$participant = 'acme_corp'
 
 # The default node definition matches any node lacking a more specific node
 # definition. If there are no other node definitions in this file, classes
