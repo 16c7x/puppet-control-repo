6d66993625f9894bd15fa2dca7e94b7647c1aeea
Andrew Jones <andrew.jones@puppet.com>
2025-05-13 16:56:02 +0100

    log level test

--- site-modules/profile/manifests/letsencrypt.pp
+++ site-modules/profile/manifests/letsencrypt.pp
@@ -1,12 +1,9 @@
 #
 class profile::letsencrypt {
   class { 'letsencrypt':
-    email => 'you@example.com',
-  }
-
-  letsencrypt::certonly { 'example.com':
-    domains       => ['example.com', 'www.example.com'],
-    plugin        => 'webroot',
-    webroot_paths => ['/var/www/html', '/var/www/html'],
+    config => {
+      email  => 'foo@example.com',
+      server => 'https://acme-v02.api.letsencrypt.org/directory',
+    },
   }
 }
