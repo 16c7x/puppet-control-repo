3e6e0982bbf30c4bbd5f4e98022ef41229122180
Andrew Jones <andrew.jones@puppet.com>
2025-05-13 17:33:21 +0100

    log level test

--- site-modules/profile/manifests/letsencrypt.pp
+++ site-modules/profile/manifests/letsencrypt.pp
@@ -1,13 +1,7 @@
 #
 class profile::letsencrypt {
-  class { 'letsencrypt':
-    email => 'admin@example.com',
-  }
-
   letsencrypt::certonly { 'puppet.example.com':
-    domains        => ['puppet.example.com'],
-    plugin         => 'webroot',
-    webroot_paths  => ['/opt/puppetlabs/server/data/console-services/public'],
-    manage_cron    => true,  # enables auto-renewal 
+    domains => ['puppet.example.com'],
+    plugin  => 'standalone',
   }
 }
