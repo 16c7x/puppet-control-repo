02106b2b6c7d0ba20245a9b3c5f98a0802e0b69f
Andrew Jones <andrew.jones@puppet.com>
2025-05-14 08:37:02 +0100

    log level test

--- site-modules/profile/manifests/letsencrypt.pp
+++ site-modules/profile/manifests/letsencrypt.pp
@@ -1,7 +1,7 @@
 #
 class profile::letsencrypt {
-  letsencrypt::certonly { 'puppet.example.com':
-    domains => ['puppet.example.com'],
+  letsencrypt::certonly { 'ec2-3-252-226-139.eu-west-1.compute.amazonaws.com':
+    domains => ['ec2-3-252-226-139.eu-west-1.compute.amazonaws.com'],
     plugin  => 'standalone',
   }
 }
